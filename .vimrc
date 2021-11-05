b78e2ac4a82f54dff34eee80f44c6f3a835c38fb
Vetle Brænd <vbr@resoptima.com>
2021-11-05 13:26:31 +0100

    Move vimrc contents to separate file

---
+++ .vimrc
@@ -0,0 +1,41 @@
+set rnu
+set encoding=utf-8
+set expandtab
+set tabstop=4
+set shiftwidth=4
+
+nnoremap <S-Up> :m-2<CR>
+nnoremap <S-Down> :m+<CR>
+inoremap <S-Up> <Esc>:m-2<CR>
+inoremap <S-Down> <Esc>:m+<CR>
+
+nnoremap <A-t> :NERDTree<CR>
+nnoremap <A-r> :NERDTreeClose<CR>
+
+autocmd BufWritePre * :%s/\s\+$//e
+
+set background=dark
+colors deus
+
+call ddc#custom#patch_global('sources', ['nvim-lsp', 'around'])
+
+call ddc#custom#patch_global('sourceOptions', {
+    \ '_': {
+    \   'matchers': ['matcher_head'],
+    \   'sorters': ['sorter_rank']},
+    \ })
+
+" <TAB>: completion.
+inoremap <silent><expr> <TAB>
+\ pumvisible() ? '<C-n>' :
+\ (col('.') <= 1 <Bar><Bar> getline('.')[col('.') - 2] =~# '\s') ?
+\ '<TAB>' : ddc#map#manual_complete()
+
+" <S-TAB>: completion back.
+inoremap <expr><S-TAB>  pumvisible() ? '<C-p>' : '<C-h>'
+
+call ddc#enable()
+
+lua << EOF
+require'lspconfig'.rnix.setup{}
+EOF
--- vim.nix
+++ vim.nix
@@ -106,49 +106,6 @@ in neovim.override {
               git-blame-nvim
           ];
         };
-        customRC = ''
-            set rnu
-            set encoding=utf-8
-            set expandtab
-            set tabstop=4
-            set shiftwidth=4
-
-            nnoremap <S-Up> :m-2<CR>
-            nnoremap <S-Down> :m+<CR>
-            inoremap <S-Up> <Esc>:m-2<CR>
-            inoremap <S-Down> <Esc>:m+<CR>
-
-            autocmd BufWritePre * :%s/\s\+$//e
-
-            set background=dark
-            colors deus
-
-            if executable('rnix-lsp')
-                au User lsp_setup call lsp#register_server({
-                    \ 'name': 'rnix-lsp',
-                    \ 'cmd': {server_info->[&shell, &shellcmdflag, 'rnix-lsp']},
-                    \ 'whitelist': ['nix'],
-                    \ })
-            endif
-
-            call ddc#custom#patch_global('sources', ['nvim-lsp', 'around'])
-
-            call ddc#custom#patch_global('sourceOptions', {
-                \ '_': {
-                \   'matchers': ['matcher_head'],
-                \   'sorters': ['sorter_rank']},
-                \ })
-
-            " <TAB>: completion.
-            inoremap <silent><expr> <TAB>
-            \ pumvisible() ? '<C-n>' :
-            \ (col('.') <= 1 <Bar><Bar> getline('.')[col('.') - 2] =~# '\s') ?
-            \ '<TAB>' : ddc#map#manual_complete()
-
-            " <S-TAB>: completion back.
-            inoremap <expr><S-TAB>  pumvisible() ? '<C-p>' : '<C-h>'
-
-            call ddc#enable()
-            '';
-        };
+        customRC = builtins.readFile ./.vimrc;
+    };
 }
