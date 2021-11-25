b86ee43b6471e60def1b6c32f9d1d31972e04813
Vetle Brænd <vbr@resoptima.com>
2021-11-25 17:00:49 +0100

    Add dotnet sdk 6

--- dotnet.nix
+++ dotnet.nix
@@ -21,7 +21,7 @@ let
       sha256 = "180kc1njpql1x227skg2v5i2nnqw05z07f1hbz1731w80j9c4wl4";
     };
   });
-   dotnet5400 = dotnetCorePackages.sdk_5_0.overrideAttrs(old: rec {
+  dotnet5400 = dotnetCorePackages.sdk_5_0.overrideAttrs(old: rec {
     version = "5.0.400";
     src = fetchDotnet {
       inherit version;
@@ -29,7 +29,16 @@ let
       sha256 = "12wj26fmp0vz11kn8524ryzd18v11v7fbnxv976nr7gh52awcz8a";
     };
   });
+  dotnet6100 = dotnetCorePackages.sdk_6_0.overrideAttrs(old: rec {
+    version = "6.0.100";
+    src = fetchDotnet {
+      inherit version;
+      name = old.pname;
+      sha256 = "hImnmPzZBKMkEdZGNuJ0ft8QgZLgtlxsPM+w0wLaXss=";
+    };
+  });
 in dotnetCorePackages.combinePackages
     [ dotnet3
       dotnet5300
-      dotnet5400 ]
+      dotnet5400
+      dotnet6100 ]
