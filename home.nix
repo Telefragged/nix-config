0df3a1c466af602e495db8f2187586520b1edb8e
Vetle Brænd <vbr@resoptima.com>
2023-03-22 15:47:22 +0100

    Enable tmux

--- home.nix
+++ home.nix
@@ -36,6 +36,31 @@
     });
   };
 
+  programs.tmux = {
+    enable = true;
+    extraConfig = ''
+      setw -g mouse on
+
+      set-window-option -g automatic-rename on
+      set-option -g set-titles on
+
+      set-option -g repeat-time 0
+
+      set -g default-terminal "screen-256color"
+
+      unbind C-b
+      set-option -g prefix C-b
+      bind-key C-b send-prefix
+
+      bind-key v split-window -h -c "#{pane_current_path}"
+      bind-key s split-window -v -c "#{pane_current_path}"
+      bind-key c new-window -c "#{pane_current_path}"
+      bind-key C-c new-window
+
+      set-option -sa terminal-overrides ',alacritty:RGB'
+    '';
+  };
+
   home.packages = with pkgs; [
     lsd
     deno
